fe08c8b7d7a38172027831f37e19ed8054415cfa
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2013-10-28 16:58:01 +0900

    Fix nkf bug#32328
    https://sourceforge.jp/ticket/browse.php?group_id=248&tid=32328

--- nkf-dist/nkf.c
+++ nkf-dist/nkf.c
@@ -6170,9 +6170,10 @@ kanji_convert(FILE *f)
 		    }
 		}
 		else {
+		    i_ungetc(c1,f);
 		    /* lonely ESC  */
 		    (*oconv)(0, ESC);
-		    SEND;
+		    SKIP;
 		}
 	    } else if (c1 == ESC && iconv == s_iconv) {
 		/* ESC in Shift_JIS */
@@ -6209,9 +6210,10 @@ kanji_convert(FILE *f)
 		    }
 		}
 		else {
+		    i_ungetc(c1,f);
 		    /* lonely ESC  */
 		    (*oconv)(0, ESC);
-		    SEND;
+		    SKIP;
 		}
 	    } else if (c1 == LF || c1 == CR) {
 		if (broken_f&4) {
